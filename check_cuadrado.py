455851015156b1ebde71bd0e23269733ea7c029a
stonarini <stonarini@cifpfbmoll.eu>
2021-10-28 19:05:20 +0200

    corregir nombre y insertar mas casos test

---
+++ check_cuadrado.py
@@ -0,0 +1,18 @@
+from caso_test_solvoku import *
+
+def check_cuadrado(sudoku):
+    for lista in sudoku:
+        if len(lista) != len(sudoku):
+            return False
+    return True
+   
+if __name__ == '__main__':
+    assert check_cuadrado(correct) == True
+    assert check_cuadrado(incorrect) == True
+    assert check_cuadrado(incorrect1) == True
+    assert check_cuadrado(incorrect2) == True 
+    assert check_cuadrado(incorrect3) == True
+    assert check_cuadrado(incorrect4) == True
+    assert check_cuadrado(incorrect5) == True
+    assert check_cuadrado(irregular) == False
+    assert check_cuadrado(irregular2) == False
