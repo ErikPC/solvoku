eba67effafd2dc32644cc494830078022eb6c412
ErikPC <epardillo@cifpfbmoll.eu>
2021-10-28 19:46:19 +0200

    comprueba que no se repita ningun numero en las columnas

---
+++ check_columnas.py
@@ -0,0 +1,25 @@
+from caso_test_solvoku import *
+
+def check_columnas(sudoku):
+    longitud = len(sudoku)
+    for i in range(longitud):
+        columna = [sudoku[j][i] for j in range(longitud)]
+# i es el index actual de la fila, j es el index actual de la columna.
+# añade a columna cada numero en el index actual de todas las filas.
+# entonces tenemos todos los index 0 de las filas almacenados en la columna.
+        for numero in columna:
+            if columna.count(numero) > 1:
+                return False
+    return True
+
+
+if __name__ == '__main__':
+    assert check_columnas(correct) == True
+    assert check_columnas(incorrect) == False
+    assert check_columnas(incorrect1) == False
+    assert check_columnas(incorrect2) == False
+    assert check_columnas(incorrect3) == True
+    assert check_columnas(incorrect4) == True
+    assert check_columnas(incorrect5) == True
+    assert check_columnas(irregular) == True
+   # assert check_columnas(irregular2) == True
