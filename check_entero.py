fbc2e9d9f3efe228daa76bd28db3df4f6a13cc6d
ErikPC <epardillo@cifpfbmoll.eu>
2021-10-28 20:38:19 +0200

    comprueba si los numero son enteros

---
+++ check_entero.py
@@ -0,0 +1,21 @@
+from caso_test_solvoku import *
+
+def check_entero(sudoku):
+    for fila in sudoku:
+        for numero in fila:
+            # comprueba si numero es un entero.
+            if not isinstance(numero, int):
+                return False
+    return True
+
+
+if __name__ == '__main__':
+    assert check_entero(correct) == True
+    assert check_entero(incorrect) == True
+    assert check_entero(incorrect1) == True
+    assert check_entero(incorrect2) == True
+    assert check_entero(incorrect3) == True
+    assert check_entero(incorrect4) == False
+    assert check_entero(incorrect5) == False
+    assert check_entero(irregular) == True
+    assert check_entero(irregular2) == True
