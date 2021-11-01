48d2a6cfc321da6127a353e57697d94bff838709
stonarini <stonarini@cifpfbmoll.eu>
2021-11-01 19:20:15 +0100

    creado check para evitar error

--- check_columnas.py
+++ check_columnas.py
@@ -6,7 +6,7 @@ def check_columnas(sudoku):
         # i es el index actual de la fila, j es el index actual de la columna.
         # añade a columna cada numero en el index actual de todas las filas.
         # entonces tenemos todos los index 0 de las filas almacenados en la columna.
-        columna = [sudoku[j][i] for j in range(longitud)]
+        columna = [sudoku[j][i] for j in range(longitud) if longitud == len(sudoku[j])]
         for numero in columna:
             if columna.count(numero) > 1:
                 return False
@@ -22,4 +22,4 @@ if __name__ == '__main__':
     assert check_columnas(incorrect4) == True
     assert check_columnas(incorrect5) == True
     assert check_columnas(irregular) == True
-   # assert check_columnas(irregular2) == True
+    assert check_columnas(irregular2) == True
